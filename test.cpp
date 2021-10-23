db628d74c06d9f76455177ee6c20054a433fff81
Afroza21 <afrozanasrin0@gmail.com>
2021-10-23 19:52:47 +0530

    First Commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    cout<<"Mew Afroza\n";
+
+    return 0;
+}
+/*
+
+git status //show untracked file or file edited
+git add --all . //shelve for change
